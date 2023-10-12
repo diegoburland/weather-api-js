bede19a6e0a1b8aaf54ea29c4148c80b2d0456a8
Diego Burlando <diegoeburlando@gmail.com>
2023-10-12 13:31:34 -0500

    api ready

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 const express = require("express");
 const axios = require("axios");
 require("dotenv").config();
-const iplocation = require("iplocation").default;
-const requestIp = require("request-ip");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 const API_KEY = process.env.API_KEY;
 
 app.get("/weather", async (req, res) => {
-  const city = req.query.city;
-
-  if (!city) {
-    return res.status(400).send({ error: "You should provide the city" });
-  }
-
   try {
-    const ip = req.clientIp;
-    const location = await iplocation(ip);
     const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=Medellín&appid=${API_KEY}`
     );
     res.send({
       city: data.name,
@@ -28,8 +17,11 @@ app.get("/weather", async (req, res) => {
       description: data.weather[0].description,
     });
   } catch (error) {
+    console.error(error);
     res.status(500).send({ error: "Hubo un error al obtener el clima." });
   }
 });
 
-app.listen;
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
